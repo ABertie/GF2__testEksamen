8142c44c1b1ec786dd30b9257e612bfa6641a04e
ABertie <U2439307@rts-365.dk>
2023-06-16 08:43:20 +0200

    carosel styling and som test styling

--- carosel.js
+++ carosel.js
@@ -3,7 +3,7 @@ const CAROSEL_FORWARD = document.querySelector(".carosel__forwardArrow");
 const CAROSEL_BACK = document.querySelector(".carosel__backArrow");
 const SPOTS = document.querySelector(".carosel__nav");
 const SLIDE = [
-    '<article class="carosel__slid"><h1>Teambuilding</h1><p>Jagtanekdoter, bramfri råvaresnak og madlavning i det fri.</p><p>Mød naturmanden Jørgen Skouboe og kokken Nikolai Kirk fra det populære tv-program “Nak og Æd”.</p><button>Læs mere</button><img src="./asset/img/COLOURBOX2002681.jpg" alt="Snobrød"></article>','<article class="carosel__slid"><h1>Event Centrum skaber<span>DEN HELT RIGTIGE STEMNING</span></h1><div class="textSeparator"></div><p>...i jeres egne rammer (kantine, lager, parkeringskælder), udendørs (skov, strand) eller på hoteller & konferencesteder overalt i Danmark.</p><button>Læs mere</button><img src="./asset/img/COLOURBOX2556345.jpg" alt="Planlægningsmøde"></article>','<article class="carosel__slid"><h1>Erfaring og enagagement</h1><p>Event Centrum har mange års erfaring med udvikling og afholdelse af både store og små arrangementer.</p><p>Hos Event Centrum stræber vi efter, at vores kunder kommer til at opleve glæde og motivation ved hjælp af ekstraordinære oplevelser. Vi vil med vores professionalisme og gode historier skabe grobund for at styrke relationer mellem ledelse og medarbejdere, virksomheden og kunderne.</p><button>Læs mere</button><img src="./asset/img/COLOURBOX548669.jpg" alt="Middelalder kriger"></article>'
+    '<article class="carosel__slide1"><div class="textField"><h1>Teambuilding</h1><p>Jagtanekdoter, bramfri råvaresnak og madlavning i det fri.</p><p>Mød naturmanden Jørgen Skouboe og kokken Nikolai Kirk fra det populære tv-program “Nak og Æd”.</p><button>Læs mere</button></div><img src="./asset/img/COLOURBOX2002681.jpg" alt="Snobrød"></article>','<article class="carosel__slide2"><div class="textField"><h1 class="SpanHeader">Event Centrum skaber<span>DEN HELT RIGTIGE STEMNING</span></h1><div class="textSeparator"></div><p>...i jeres egne rammer (kantine, lager, parkeringskælder), udendørs (skov, strand) eller på hoteller & konferencesteder overalt i Danmark.</p><button class="buttonBorder">Læs mere</button></div><img src="./asset/img/COLOURBOX2556345.jpg" alt="Planlægningsmøde"></article>','<article class="carosel__slide3"><div class="textField"><h1>Erfaring og enagagement</h1><p>Event Centrum har mange års erfaring med udvikling og afholdelse af både store og små arrangementer.</p><p>Hos Event Centrum stræber vi efter, at vores kunder kommer til at opleve glæde og motivation ved hjælp af ekstraordinære oplevelser. Vi vil med vores professionalisme og gode historier skabe grobund for at styrke relationer mellem ledelse og medarbejdere, virksomheden og kunderne.</p><button class="buttonBorder">Læs mere</button></div><img src="./asset/img/COLOURBOX548669.jpg" alt="Middelalder kriger"></article>'
 ];
 
 var index = 0
@@ -23,9 +23,9 @@ CAROSEL_SLIDER.addEventListener("animationend", disabelAnimation)
 function highlightButton() {
     BUTTONS.forEach(function (button, i) {
         if (index === i) {
-            button.classList.add('highlight')
+            button.classList.add('carosel__highlightButton')
         } else {
-            button.classList.remove('highlight')
+            button.classList.remove('carosel__highlightButton')
         }
         button.addEventListener("click", function () {
             if (index < i) {
@@ -77,5 +77,5 @@ function disabelAnimation() {
     
 }
 
-window.setInterval(shuffleForward, 60000)
+window.setInterval(shuffleForward, 90000)
     
